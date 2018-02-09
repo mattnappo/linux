f20df248afa8fbe2dff62ea10a36483220b63bcc
Matt Nappo <bolt@Matts-MacBook-Pro.local>
2018-02-09 08:52:07 -0500

    cleanup

---
+++ hac.py
@@ -0,0 +1,8 @@
+import tools, time, random
+f = input("What to hack, sir? ")
+print("Hacking " + f)
+time.sleep(1)
+for x in range(600000):
+	print(tools.sha256(str(random.random())))
+print(f + " has been fully  H A C K E D")
+print("虞煙ゝ媛欧シ移が")
